5ef8e071cd7dd4026aafdd228afb2ac70d5473e8
MFDOOM2000 <getdebamal100@gmail.com>
2024-09-26 14:24:00 +0530

    Update newfile.java

--- newfile.java
+++ newfile.java
@@ -1,10 +1,11 @@
 //Number Programs 
 
-//Fascinating number
-
 /*import java.util.*;
-public class Facnumber
+public class Numprogs
 {
+
+//Fascinating number 
+  
   public boolean fascinating(int n)
   {
     String s=""+n+(2*n)+(3*n);
@@ -15,32 +16,9 @@ public class Facnumber
     }
     return true;
   }
-  public static void main(String args[])
-  {
-    Facnumber obj=new Facnumber();
-    Scanner sc= new Scanner(System.in);
-    System.out.println("Enter the lower boundary value");
-    int a = sc.nextInt();
-    System.out.println("Enter the upper boundary value");
-    int b = sc.nextInt();
-    if(a<100 || b<a)
-    System.out.println("Invalid output");
-    else
-    {
-      for(int i=a; i<=b; i++)
-      {
-        if(obj.fascinating(i))
-        System.out.println(i);
-        }
-    }
-  }
-}*/
 
 //Evil number
 
-/*import java.util.*;
-public class Evilnumber
-{
     public boolean evil(int n)
     {
         int c=0;
@@ -55,32 +33,9 @@ public class Evilnumber
         else
         return false;
     }
-    public static void main (String args[])
-    {
-        Evilnumber obj=new Evilnumber();
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter the lower boundary value");
-        int a = sc.nextInt();
-        System.out.println("Enter the upper boundary value");
-        int b = sc.nextInt();
-        if(b<a)
-        System.out.println("Invalid output");
-        else
-       {
-           for(int i=a; i<=b; i++)
-          {
-              if(obj.evil(i))
-              System.out.println(i);
-              }
-       }
-    }
-}*/
 
 //Armstrong number 
 
-/*import java.util.*;
-public class Armstrong
-{
     public boolean armnum(int n)
     {
         int m=n,c=0;
@@ -101,32 +56,9 @@ public class Armstrong
         else
         return false;
     }
-    public static void main (String args[])
-    {
-        Armstrong obj=new Armstrong();
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter the lower boundary value");
-        int a = sc.nextInt();
-        System.out.println("Enter the upper boundary value");
-        int b = sc.nextInt();
-        if(b<a)
-        System.out.println("Invalid output");
-        else
-       {
-           for(int i=a; i<=b; i++)
-          {
-              if(obj.armnum(i))
-              System.out.println(i);
-              }
-       }
-    }
-}*/
-
+    
 //Pronic number
 
-/*import java.util.*;
-public class Pronic
-{
     public boolean pronicnum(int n)
     {
         for(int i=1;i<n;i++)
@@ -138,32 +70,9 @@ public class Pronic
         }
         return false;
     }
-    public static void main (String args[])
-    {
-        Pronic obj=new Pronic();
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter the lower boundary value");
-        int a = sc.nextInt();
-        System.out.println("Enter the upper boundary value");
-        int b = sc.nextInt();
-        if(b<a)
-        System.out.println("Invalid output");
-        else
-       {
-           for(int i=a; i<=b; i++)
-          {
-              if(obj.pronicnum(i))
-              System.out.println(i);
-           }
-       }
-    }
-}*/
-
+    
 //Smith numbers
 
-/*import java.util.*;
-public class Smithnum
-{
     public int sumOfDigits (int n)
     {
         int s=0;
@@ -199,32 +108,9 @@ public class Smithnum
         else
         return false;
     }
-    public static void main (String args[])
-    {
-        Smithnum obj=new Smithnum();
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter the lower boundary value");
-        int a = sc.nextInt();
-        System.out.println("Enter the upper boundary value");
-        int b = sc.nextInt();
-        if(b<a)
-        System.out.println("Invalid output");
-        else
-       {
-           for(int i=a; i<=b; i++)
-          {
-              if(obj.isComposite(i) && obj.smith(i))
-              System.out.println(i);
-           }
-       }
-    }
-}*/
-
+    
 //Dudeney numbers
 
-/*import java.util.*;
-public class DudeNum
-{
      public boolean dudeney(int n)
      {
          int m=n,s=0;
@@ -238,32 +124,9 @@ public class DudeNum
          else
          return false;
      }
-     public static void main (String args[])
-    {
-        DudeNum obj=new DudeNum();
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter the lower boundary value");
-        int a = sc.nextInt();
-        System.out.println("Enter the upper boundary value");
-        int b = sc.nextInt();
-        if(b<a)
-        System.out.println("Invalid output");
-        else
-       {
-           for(int i=a; i<=b; i++)
-          {
-              if(obj.dudeney(i))
-              System.out.println(i);
-           }
-       }
-    }
-}*/
-
+     
 //Bouncy number 
 
-/*import java.util.*;
-public class Bouncynum
-{
     public boolean bouncy(int n)
     {
         int prev=2;//dummy number
@@ -278,32 +141,9 @@ public class Bouncynum
         }
         return false;
     }
-    public static void main (String args[])
-    {
-        Bouncynum obj=new Bouncynum();
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter the lower boundary value");
-        int a = sc.nextInt();
-        System.out.println("Enter the upper boundary value");
-        int b = sc.nextInt();
-        if(b<a || a<100)
-        System.out.println("Invalid output");
-        else
-       {
-           for(int i=a; i<=b; i++)
-          {
-              if(obj.bouncy(i))
-              System.out.println(i);
-           }
-       }
-    }
-}*/
-
+    
 //Abundant numbers 
 
-/*import java.util.*;
-public class Abnum
-{
     public boolean abundant(int n)
     {
         int s=0;
@@ -317,32 +157,9 @@ public class Abnum
         else
         return false;
     }
-    public static void main (String args[])
-    {
-        Abnum obj=new Abnum();
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter the lower boundary value");
-        int a = sc.nextInt();
-        System.out.println("Enter the upper boundary value");
-        int b = sc.nextInt();
-        if(b<a)
-        System.out.println("Invalid output");
-        else
-       {
-           for(int i=a; i<=b; i++)
-          {
-              if(obj.abundant(i))
-              System.out.println(i);
-           }
-       }
-    }
-}*/
-
+    
 //Spy number
 
-/*import java.util.*;
-public class Spynum
-{
     public boolean spy(int n)
     {
         int m=n,s=0,p=1;
@@ -357,32 +174,9 @@ public class Spynum
         else
         return false;
     }
-    public static void main (String args[])
-    {
-        Spynum obj=new Spynum();
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter the lower boundary value");
-        int a = sc.nextInt();
-        System.out.println("Enter the upper boundary value");
-        int b = sc.nextInt();
-        if(b<a)
-        System.out.println("Invalid output");
-        else
-       {
-           for(int i=a; i<=b; i++)
-          {
-              if(obj.spy(i))
-              System.out.println(i);
-           }
-       }
-    }
-}*/
-
+    
 //Prime palindrome number
 
-/*import java.util.*;
-public class PrimePalin
-{
     public boolean prime(int n)
     {
         for(int i=2;i<n;i++)
@@ -416,8 +210,7 @@ public class PrimePalin
         else
        {
            for(int i=a; i<=b; i++)
-          {
-              if(obj.palindrome(i) && obj.prime(i))
+           {
               System.out.println(i);
            }
        }
